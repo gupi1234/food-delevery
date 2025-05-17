545303a869f3edcf24432cfc98e81dac2aa649ed
gupi debnath <iamgupidebntah@gmail.com>
2025-05-17 10:46:19 +0530

    Fix case mismatch in FoodItem import

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
-import FoodItem from "../FoodItem/foodItem";
+import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+
   return (
-    <div className="food-display" id=" food-display">
-      <h2 className="">Top Dishes near you</h2>
+    <div className="food-display" id="food-display">
+      <h2>Top Dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          if (category === "All" || item.category === category)
+          if (category === "All" || item.category === category) {
             return (
               <FoodItem
                 key={index}
@@ -21,6 +22,8 @@ const FoodDisplay = ({ category }) => {
                 image={item.image}
               />
             );
+          }
+          return null;
         })}
       </div>
     </div>
